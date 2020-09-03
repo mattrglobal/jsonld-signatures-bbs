610af3583e0a332ec4ef5341edf19baf97a06235
Colin Yip <colin.yip@mattr.global>
2020-09-03 12:51:11 +1200

    feat: export types

--- src/index.ts
+++ src/index.ts
@@ -14,5 +14,6 @@
 export { Bls12381G2KeyPair } from "@mattrglobal/bls12381-key-pair";
 export { BbsBlsSignature2020 } from "./BbsBlsSignature2020";
 export { BbsBlsSignatureProof2020 } from "./BbsBlsSignatureProof2020";
+export * from "./types";
 
 export { deriveProof } from "./deriveProof";
